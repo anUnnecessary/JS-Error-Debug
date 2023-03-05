1bad11d0a5014afd25585dc8549a6c407f681687
Abdullah Raihan Shamil <raihanshamil33@gmail.com>
2023-03-05 21:20:14 +0600

    sec

--- type-error.js
+++ type-error.js
@@ -2,6 +2,14 @@ const numb = 4;
 // numb.push(45);
 
 
-let address ;
+// let address ;
 
-console.log(address.home)
+// console.log(address.home)
+let address ={ city: 'bibaria',postCode:6900};
+// console.log(address.home.building)
+console.log(address?.home?.building)
+
+
+
+const a = 'abul';
+a = 'babul';
