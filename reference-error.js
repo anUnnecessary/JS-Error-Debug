ef03bbb7ded262e476cb9bf9dd754426bd951901
Abdullah Raihan Shamil <raihanshamil33@gmail.com>
2023-03-05 21:40:50 +0600

    third

---
+++ reference-error.js
@@ -0,0 +1,23 @@
+const x ='pori'
+const y = 'boy'
+// console.log(z)
+
+
+
+// console.log(name)
+// const name = 'some boys'
+// var name = 'some boys'
+// let name = 'some boys'
+
+// console.log(add2(2,3))
+function add(n1,m2){
+    return n1+ m2
+}
+const add2 =(n1,n2) => n1+n2
+
+
+function doSomething (){
+    const sum = 50
+
+}
+// console.log(sum)
